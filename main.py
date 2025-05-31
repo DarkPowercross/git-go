d0c31155297844487fb44e7e67a7f53e791e4589
Alec Pratt <prattalec@gmail.com>
2025-05-31 11:31:14 +0200

    config update

--- main.py
+++ main.py
@@ -10,14 +10,23 @@ def main():
     subparsers = parser.add_subparsers(dest="command", required=True)
     connect_parser = subparsers.add_parser("connect", help="connect to server")
     connect_parser.add_argument("profile", help="The profile name to connect to")
+
+    add_parser = subparsers.add_parser("add", help="connect to server")
+    list_parser = subparsers.add_parser("list", help="connect to server")
+    delete_parser = subparsers.add_parser("delete", help="connect to server")
+    # add_parser.add_argument("profile", help="The profile name to connect to")
+
     args = parser.parse_args()
 
     if args.command == "connect":
-        print(args.profile)
         data = query_data(args.profile)
         ssh_connect(data["username"], data["host"], data["identity"], data["port"])     
-    elif args.test:
-        print(args.test)
+    elif args.command == "add":
+        add_profile()
+    elif args.command == "delete":
+        del_profile()
+    elif args.command == "list":
+        view_profile()
     else:
         print("hello")
 
--- sshgo/query.py
+++ sshgo/query.py
@@ -1,5 +1,5 @@
 import os
-
+import sys
 from sshgo.variables import *
 from ruamel.yaml import YAML
 
@@ -10,3 +10,78 @@ def query_data(profile):
         data = yaml.load(f)
 
     return data['profiles'][profile]
+
+def add_profile():
+    yaml = YAML()
+    yaml.preserve_quotes = True
+
+    with open(FILE_PATH, "r") as file:
+        data = yaml.load(file)
+
+    while True:
+        alias = input(f"{'Server Alias':<20}|  ")
+        if alias in data['profiles']:
+            print("Name already exists")
+            continue
+        host = input(f"{'host':<20}|  ")
+
+        port = input(f"{'port':<20}|  ")
+        username = input(f"{'username':<20}|  ")
+        identity = input(f"{'identity:':<20}|  ")
+        
+        break
+
+    if 'profiles' not in data or not isinstance(data['profiles'], dict):
+        data['profiles'] = {}
+
+    data['profiles'][alias] = {
+        'host': host,
+        'port': port,
+        'username': username,
+        'identity': identity
+    }
+
+    with open(FILE_PATH, 'w') as file:
+        yaml.dump(data, file)
+
+    print(f"Profile '{alias}' added successfully")
+
+def del_profile():
+    yaml = YAML()
+    yaml.preserve_quotes = True
+
+    with open(FILE_PATH, "r") as file:
+        data = yaml.load(file)
+
+    while True:
+        alias = input(f"{'Server Alias':<20}|  ")
+        if alias not in data['profiles']:
+            print("Name does not exists")
+            continue
+        break
+
+    del data['profiles'][alias]
+
+    with open(FILE_PATH, 'w') as file:
+        yaml.dump(data, file)
+
+    print(f"Profile '{alias}' removed successfully")
+
+def view_profile():
+    yaml = YAML()
+    yaml.preserve_quotes = True
+
+    with open(FILE_PATH, "r") as file:
+        data = yaml.load(file)
+
+    profiles = data.get('profiles', {})
+
+    if not profiles:
+        print("No profiles found.")
+        return
+
+    print(f"\n{'Alias':<20} | {'Host':<15} | {'Port':<5} | {'Username':<10} | {'Identity'}")
+    print("-" * 70)
+
+    for alias, details in profiles.items():
+        print(f"{alias:<20} | {details.get('host', ''):<15} | {details.get('port', ''):<5} | {details.get('username', ''):<10} | {details.get('identity', '')}")
